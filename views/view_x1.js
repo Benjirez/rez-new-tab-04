31b4af5cca6b6a5a3d2013170349002e2756f980
Benjirez <benjirez@gmail.com>
2023-09-17 13:46:30 -0700

    touch up and removed fat

--- views/view_x1.js
+++ views/view_x1.js
@@ -1,25 +1,15 @@
-//var Mousetrap = require('mousetrap');
-//var copy = require('clipboard-copy');
-
-//var Mousetrap = rezBundle.Mousetrap;
-//var copy = rezBundle.copy;
-
 $(document).ready(function () {
-  //console.log( myData[0] );
-  //var myData = xsvr.locals.myData;
   var sel1 = $("#sel1");
   var selected_id;
 
   sel1.attr("multiple", true).attr("size", 16);
 
-  //$("select option[value='0']").attr("selected","selected"); //.changed();
-
   function doIt(filterData) {
     console.log("listen R2 they're dying");
-    //sel1.selectedIndex
+
     selTx = $("#sel1 option:selected").val();
     selected_id = filterData[selTx]._id;
-    //console.log( selTx );
+
     $("#myTitle").val(filterData[selTx].col_a);
     $("#myHK").val(filterData[selTx].col_b);
     $("#col2").html(filterData[selTx].col_c);
@@ -104,73 +94,4 @@ $(document).ready(function () {
   $("#filters").on("submit", function () {
     console.log("g-filter");
   });
-
-  //$( "#sel1 option:selected"
-  /*
-  $('#form1').on('submit', function(){
-	console.log('submitting yo');
-      var item = $('#form1 input');
-      var todoX = {item: item.val()};
-
-      $.ajax({
-        type: 'POST',
-        url: '/',
-        data: todoX,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-
-      return false;
-
-  });
-
-   $('#form2').on('submit', function(){
-	console.log('submitting 2 yo');
-      var item = $('#form2 input');
-      var todoX = {item: item.val()};
-
-      $.ajax({
-        type: 'POST',
-        url: '/form2',
-        data: todoX,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-
-      return false;
-
-  });
-
-  $('li').on('click', function(){
-      var item = $(this).attr('name'); //.replace(/ /g, "-");
-	  console.log(item);
-      $.ajax({
-        type: 'DELETE',
-        url: '/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-
-        }
-      });
-  });
-
-  */
-
-  /*
-	$.contextMenu({
-		// define which elements trigger this menu
-		selector: "#pork",
-		// define the elements of the menu
-		items: {
-			foo: {name: "Foo", callback: function(key, opt){ alert("Foo!"); }},
-			bar: {name: "Bar", callback: function(key, opt){ alert("Bar!") }}
-		}
-		// there's more, have a look at the demos and docs...
-	});
-		*/
 });
